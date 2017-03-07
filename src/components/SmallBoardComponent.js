24c4dc49e38d7c21208ef1c8c91df433d2c3bc14
Og Astorga <og.asdiaz@gmail.com>
2017-03-06 20:08:50 -0600

    Calc smallBoard's winners and tag the winners with a class

--- src/components/SmallBoardComponent.js
+++ src/components/SmallBoardComponent.js
@@ -4,12 +4,78 @@ import React, { PropTypes } from 'react';
 
 require('styles//SmallBoard.css');
 
+let calcBoardWinner = (board) => {
+  board = board.join('');
+
+  let winner;
+
+  // Horizontal lines
+  for (let i=0; i<3; ++i) {
+    winner = board[i*3];
+    for (let j=0; j<3; ++j) {
+      if (winner !== board[i*3 + j]) {
+        winner = '.';
+      }
+    }
+
+    if (winner !== '.') {
+      return winner;
+    }
+  }
+
+  // Vertical lines
+  for (let i=0; i<3; ++i) {
+    winner = board[i];
+    for (let j=0; j<3; ++j) {
+      if (winner !== board[i + j*3]) {
+        winner = '.';
+      }
+    }
+
+    if (winner !== '.') {
+      return winner;
+    }
+  }
+
+  // Diagonals
+  winner = board[0];
+  for (let i=0; i<3; ++i) {
+    if (winner !== board[i*3 + i]) {
+      winner = '.';
+    }
+  }
+
+  if (winner !== '.') {
+    return winner;
+  }
+
+  winner = board[0 + 2];
+  for (let i=0; i<3; ++i) {
+    if (winner !== board[i*3 + (2-i)]) {
+      winner = '.';
+    }
+  }
+
+  if (winner !== '.') {
+    return winner;
+  }
+
+  return '.';
+}
+
 let SmallBoardComponent = (props) => {
   const { board } = props;
 
+  const boardWinner = calcBoardWinner(board);
+
+  let className = 'smallboard-component';
+  if (boardWinner !== '.') {
+    className += ` player-${boardWinner}`;
+  }
+
   return (
-    <div className="smallboard-component">
-      <pre>{board}</pre>
+    <div className={className}>
+      <pre>{board.join('\n')}</pre>
     </div>
   );
 }
